e3f97b0d2a19b697f64cddf4e3899829aa902442
jiyeon lee <a1919361@gmail.com>
2021-01-17 12:19:45 +0900

    add physics by using useCannon

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import {Canvas} from 'react-three-fiber';
 import {OrbitControls, Stars} from 'drei';
+import {Physics, useBox, usePlane} from "use-cannon"; // pysics library : cannon js
 import './style.css';
 
 function Box() {
+  const [ref] = useBox(() => ({mass: 1,  position:[0,2,0]}));
   return (
-    <mesh>
+    <mesh ref={ref}>
       <boxBufferGeometry attach="geometry"/>
       <meshLambertMaterial attach="material" color="hotpink"/>
+    </mesh>
+  )
+}
+
+function Plane() {
+  const [ref] = usePlane(() => ({rotation:[-Math.PI /2,0,0] }));
+  return (
+    <mesh rotation={[-Math.PI /2,0,0]}>
+      <planeBufferGeometry attach="geometry" args={[100,100]}/>
+      <meshLambertMaterial attach="material" color="lightblue"/>
     </mesh>
   )
 }
@@ -18,9 +30,14 @@ export default function App() {
     <Stars />
     <ambientLight intensity={0.5}/>
     <spotLight
-    position={[10,14,10]}
-    angle={0.3}
+      position={[10,14,10]}
+      angle={0.3}
     />
-    <Box/>
+    <Physics>
+      <Box/>
+      <Plane/>
+    </Physics>
+    
+    
   </Canvas>);
 }
