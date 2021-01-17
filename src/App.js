a99066de30fee9f250fde9ec10fd0a241e46cc47
jiyeon lee <a1919361@gmail.com>
2021-01-17 12:09:59 +0900

    add starry sky

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Canvas} from 'react-three-fiber';
-import {OrbitControls} from 'drei';
+import {OrbitControls, Stars} from 'drei';
 import './style.css';
 
 function Box() {
   return (
     <mesh>
       <boxBufferGeometry attach="geometry"/>
-      <meshLambertMaterial attach="meterial" color="hotpink"/>
+      <meshLambertMaterial attach="material" color="hotpink"/>
     </mesh>
   )
 }
@@ -15,7 +15,12 @@ export default function App() {
   return (
   <Canvas>
     <OrbitControls/>
+    <Stars />
     <ambientLight intensity={0.5}/>
+    <spotLight
+    position={[10,14,10]}
+    angle={0.3}
+    />
     <Box/>
   </Canvas>);
 }
