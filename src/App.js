d94becec7eced16fc1cf80f3ea7db8fedd82a90c
jiyeon lee <a1919361@gmail.com>
2021-01-17 12:00:49 +0900

    add box

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React from 'react';
 import {Canvas} from 'react-three-fiber';
 import './style.css';
 
+function Box() {
+  return (
+    <mesh>
+      <boxBufferGeometry attach="geometry"/>
+      <meshLambertMaterial attach="meterial" color="hotpink"/>
+    </mesh>
+  )
+}
 export default function App() {
-  return <Canvas></Canvas>;
+  return (
+  <Canvas>
+    <Box/>
+  </Canvas>);
 }
